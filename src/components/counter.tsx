b43cf125f101f119f16a049c7f2d0a02ee29a5a1
Stephen Zhou <hi@hyoban.cc>
2024-01-23 18:22:21 +0800

    useLocation

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { useState } from 'react';
+import { useLocation } from 'waku/router/client';
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
 
   const handleIncrement = () => setCount((c) => c + 1);
+
+  const location = useLocation()
 
   return (
     <section className="border-blue-400 -mx-4 mt-4 rounded border border-dashed p-4">
@@ -14,7 +17,7 @@ export const Counter = () => {
         onClick={handleIncrement}
         className="rounded-sm bg-black px-2 py-0.5 text-sm text-white"
       >
-        Increment
+        Increment {location.path}
       </button>
     </section>
   );
